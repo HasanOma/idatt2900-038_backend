3a3968c430f8bc47dbb03b4d6ebe9f29ee61e749
Hasan Rehman Omarzae <hasanro@stud.ntnu.no>
2023-02-08 15:29:20 +0100

    Weather API ready.

--- MTP_038_backend/api_weather.py
+++ MTP_038_backend/api_weather.py
@@ -1,10 +1,20 @@
 import asyncio
 import aiohttp
-import requests
 import asgiref
-from MTP_038_backend import models
+import requests
+async def weather_api():
 
 
-async def weather_by_coordinates():
-    latitude = 63.46
-    longitude = 10.38
+    url = "https://api.met.no/weatherapi/locationforecast/2.0/compact"
+
+    querystring = {"altitude": "5", "lat": "63.46", "lon": "10.38"}
+
+    payload = ""
+    headers = {
+        "User-Agent": "Marine_Traffic_Portal_BachelorOppgaveNTNU/1.0"
+    }
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+    print(response.text)
+
+asyncio.run(weather_api())
