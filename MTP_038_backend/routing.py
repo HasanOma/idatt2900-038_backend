ca1151dd779c8d79baad71ab6e44132d2ae3e2d8
Hasan Rehman Omarzae <hasanro@stud.ntnu.no>
2023-02-14 09:19:16 +0100

    Websocket Routing added for ship locations.

--- MTP_038_backend/routing.py
+++ MTP_038_backend/routing.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 from MTP_038_backend import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/ship_location/', consumers.ShipLocationConsumer.as_asgi()),
+    re_path(r'ws/filtered_ships/', consumers.Filtered_Ships.as_asgi()),
+    re_path(r'ws/ship_locations/', consumers.Filtered_Ships.as_asgi()),
     re_path(r'ws/weather/', consumers.Weather_data.as_asgi())
 ]
