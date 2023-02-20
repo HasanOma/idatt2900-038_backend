d2c90ea846a932de082b447d1f19e6c4443a8c99
Hasan Rehman Omarzae <hasanro@stud.ntnu.no>
2023-02-20 18:59:32 +0100

    ship model added.

--- MTP_038_backend/models.py
+++ MTP_038_backend/models.py
@@ -45,6 +45,65 @@ class Vessel:
         self.positionFixingDeviceType = data['properties']['positionFixingDeviceType']
         self.reportClass = data['properties']['reportClass']
 
+class Ship(Model):
+    mmsi = fields.IntField(primary_key=True)
+    name = fields.CharField(max_length=255, null = True)
+    msgtime = fields.CharField(max_length=255, null=True)
+    latitude = fields.FloatField(null=True)
+    longitude = fields.FloatField(null=True)
+    speedOverGround = fields.FloatField(null=True)
+    courseOverGround = fields.FloatField(null=True)
+    navigationalStatus = fields.IntField(null=True)
+    rateOfTurn = fields.FloatField(null=True)
+    shipType = fields.IntField(null=True)
+    trueHeading = fields.IntField(null=True)
+    callSign = fields.CharField(max_length=255, null=True)
+    destination = fields.CharField(max_length=255, null=True)
+    eta = fields.CharField(max_length=255, null=True)
+    imoNumber = fields.IntField(null=True)  # Add this line
+    dimensionA = fields.IntField(null=True)
+    dimensionB = fields.IntField(null=True)
+    dimensionC = fields.IntField(null=True)
+    dimensionD = fields.IntField(null=True)
+    draught = fields.FloatField(null=True)
+    shipLength = fields.FloatField(null=True)
+    shipWidth = fields.FloatField(null=True)
+    positionFixingDeviceType = fields.IntField(null=True)
+    reportClass = fields.CharField(max_length=255, null=True)
+
+    def save_to_database(self):
+        # Check if the object already exists in the database
+        try:
+            ship = Ship.objects.get(mmsi=self.mmsi)
+        except Ship.DoesNotExist:
+            # If the object does not exist, create a new one
+            ship = Ship(mmsi=self.mmsi)
+
+        # Update the object attributes
+        ship.imoNumber = self.imoNumber
+        ship.name = self.name
+        ship.msgtime = self.msgtime
+        ship.speedOverGround = self.speedOverGround
+        ship.courseOverGround = self.courseOverGround
+        ship.navigationalStatus = self.navigationalStatus
+        ship.rateOfTurn = self.rateOfTurn
+        ship.shipType = self.shipType
+        ship.trueHeading = self.trueHeading
+        ship.callSign = self.callSign
+        ship.destination = self.destination
+        ship.eta = self.eta
+        ship.dimensionA = self.dimensionA
+        ship.dimensionB = self.dimensionB
+        ship.dimensionC = self.dimensionC
+        ship.dimensionD = self.dimensionD
+        ship.draught = self.draught
+        ship.shipLength = self.shipLength
+        ship.shipWidth = self.shipWidth
+        ship.positionFixingDeviceType = self.positionFixingDeviceType
+        ship.reportClass = self.reportClass
+
+        # Save the updated or new object to the database
+        ship.save()
 
 class Weather:
     def __init__(self,weather_data):
