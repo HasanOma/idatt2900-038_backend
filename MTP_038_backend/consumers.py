1accb31f953b032f412570f1af5a60bfe9e592ae
Hasan Rehman Omarzae <hasanro@stud.ntnu.no>
2023-03-05 17:29:57 +0100

    Websocket stops running when client disconnects.

--- MTP_038_backend/consumers.py
+++ MTP_038_backend/consumers.py
@@ -7,44 +7,63 @@ from MTP_038_backend import api_stream
 # from backend.database import session
 
 class Filtered_Ships(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.is_running = False
+
     async def connect(self):
         await self.accept()
         await api_stream.main()
-        while True:
+        self.is_running = True
+        while self.is_running:
             message = await api_stream.filter_ships()
             print(message)
             await self.send(text_data=json.dumps({
                 'message': message
             }))
-        # await api_stream.filter_ships()
+            # Add a delay to avoid sending messages too frequently
+            await asyncio.sleep(1)
 
     async def disconnect(self, close_code):
-        pass
+        self.is_running = False
+
 
 class Ship_locations(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.is_running = False
+
     async def connect(self):
         await self.accept()
         await api_ship_requests.main()
-        while True:
+        self.is_running = True
+        while self.is_running:
             message = await api_ship_requests.all_ships()
             print(message)
             await self.send(text_data=json.dumps({
                 'message': message
             }))
             # Add a delay to avoid sending messages too frequently
+            await asyncio.sleep(1)
 
     async def disconnect(self, close_code):
-        pass
+        self.is_running = False
 
 class Weather_data(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.is_running = False
+
     async def connect(self):
         await self.accept()
-        while True:
+        self.is_running = True
+        while self.is_running:
             msg = await api_weather.weather_api()
             print(msg, " 2")
             await self.send(text_data=json.dumps({
                 'weather': msg
             }))
             await asyncio.sleep(900)
+
     async def disconnect(self, code):
-        pass
+        self.is_running = False
