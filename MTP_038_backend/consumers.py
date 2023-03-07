0da38293d5fd8bac9d4b619d2a831b0b52794f35
Hasan Rehman Omarzae <hasanro@stud.ntnu.no>
2023-03-07 16:08:05 +0100

    Group messaging logic added to ships to send frontend, so that we dont overdo our API requests.

--- MTP_038_backend/consumers.py
+++ MTP_038_backend/consumers.py
@@ -6,45 +6,113 @@ from MTP_038_backend import api_weather
 from MTP_038_backend import api_stream
 from backend.database import session
 
+class Ship_locations(AsyncWebsocketConsumer):
+    group_name = 'ship_locations_group'
+
+    async def connect(self):
+        await self.accept()
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        self.is_running = True
+        await self.send_current_location()
+        if len(self.channel_layer.groups[self.group_name]) == 1:
+            asyncio.create_task(self.send_ship_locations())
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    async def send_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+    async def send_current_location(self):
+        await api_ship_requests.main()
+        message = await api_ship_requests.all_ships()
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+    async def send_ship_locations(self):
+        while self.is_running:
+            message = await api_ship_requests.all_ships()
+            print(message)
+            await self.channel_layer.group_send(
+                self.group_name,
+                {
+                    'type': 'send_message',
+                    'message': message
+                }
+            )
+
+            await asyncio.sleep(1)
+
+
+
 class Filtered_Ships(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.is_running = False
+
     async def connect(self):
         await self.accept()
         await api_stream.main()
-        while True:
-            message = await api_stream.filter_ships()
-            print(message)
-            await self.send(text_data=json.dumps({
-                'message': message
-            }))
-        # await api_stream.filter_ships()
+        self.is_running = True
+        while self.is_running:
+            await api_stream.filter_ships()
 
     async def disconnect(self, close_code):
-        pass
-
-class Ship_locations(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
-        await api_ship_requests.main()
-        while True:
-            message = await api_ship_requests.all_ships()
-            print(message)
-            await self.send(text_data=json.dumps({
-                'message': message
-            }))
-            # Add a delay to avoid sending messages too frequently
-
-    async def disconnect(self, close_code):
-        pass
+        self.is_running = False
 
 class Weather_data(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.is_running = False
+
     async def connect(self):
         await self.accept()
-        while True:
+        self.is_running = True
+        while self.is_running:
             msg = await api_weather.weather_api()
             print(msg, " 2")
             await self.send(text_data=json.dumps({
                 'weather': msg
             }))
             await asyncio.sleep(900)
+
     async def disconnect(self, code):
-        pass
+        self.is_running = False
+
+# class Ship_locations(AsyncWebsocketConsumer):
+#     group_name = 'ship_locations_group'
+#
+#     async def connect(self):
+#         await self.accept()
+#         await self.channel_layer.group_add(self.group_name, self.channel_name)
+#         self.is_running = True
+#         if len(self.channel_layer.groups[self.group_name]) == 1:
+#             await self.send_ship_locations()
+#
+#     async def disconnect(self, close_code):
+#         await self.channel_layer.group_discard(self.group_name, self.channel_name)
+#
+#     async def send_message(self, event):
+#         message = event['message']
+#         await self.send(text_data=json.dumps({
+#             'message': message
+#         }))
+#
+#     async def send_ship_locations(self):
+#         await api_ship_requests.main()
+#         while self.is_running:
+#             message = await api_ship_requests.all_ships()
+#             print(message)
+#             await self.channel_layer.group_send(
+#                 self.group_name,
+#                 {
+#                     'type': 'send_message',
+#                     'message': message
+#                 }
+#             )
+#
+#             await asyncio.sleep(1)
